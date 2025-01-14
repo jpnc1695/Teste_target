5a98c9362ce84fc69bf532c5c910e9fd43a0f603
jpnc1695 <joaopaulonunescosta@gmail.com>
2025-01-14 09:11:31 -0300

    atualizacao

--- pergunta_5_2025.js
+++
@@ -1,8 +0,0 @@
-const str = "Hello World!";
-function reverterString (x){
-    return Array.from(x).reduce((inverter, c) => c + inverter, "")
-}
-
-var strInversa = reverterString(str)
-
-console.log(strInversa )
